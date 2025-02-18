77e61e572292806c254f42be2339bf499f5bb974
jayeew <962326693@qq.com>
2025-02-18 20:10:13 +0800

    测试test.py的参数模糊传递

--- tools/dataset_tools.py
+++ tools/dataset_tools.py
@@ -13,22 +13,22 @@ from torchvision.datasets import CIFAR10
 from utils.registry import registry
 
 class DatasetParamSchema(BaseModel):
-    batchsize: int = Field(32, title="batchsize", description="加载数据集的批次大小")
-    cifar10_path: str = Field("E:\\tmp\\cifar10", title="dataset_path", description="数据集的存储路径")
+    batch_size: int = Field(32, title="batch_size", description="加载数据集的批次大小")
+    dataset_path: str = Field("C:\\workspace\\LLMART\\datasets\\cifar10", title="dataset_path", description="数据集的存储路径")
 
 @registry.register_data('cifar10_tool')
-def cifar10_loader(batchsize: int = 32, dataset_path: str = "E:\\tmp\\cifar10") -> DataLoader: 
+def cifar10_loader(batch_size: int = 32, dataset_path: str = "C:\\workspace\\LLMART\\datasets\\cifar10") -> DataLoader: 
     '''The function to create cifar10 dataloader.'''
     transform = transforms.Compose([transforms.ToTensor()])
     cifar = CIFAR10(root=dataset_path, train=False, download=True, transform=transform)
-    data_loader = DataLoader(cifar, batch_size=batchsize, shuffle=False, num_workers=1, pin_memory= False, drop_last= False)
+    data_loader = DataLoader(cifar, batch_size=batch_size, shuffle=False, num_workers=1, pin_memory= False, drop_last= False)
     data_loader.name = "cifar10"
-    data_loader.batch = batchsize
+    data_loader.batch = batch_size
     return data_loader
 
 cifar10_tool = StructuredTool(
     name = "cifar10_tool",
-    description = "批次大小和数据集的存储路径创建cifar10数据集加载器",
+    description = "根据加载数据集的批次大小和数据集的存储路径创建cifar10数据集加载器",
     args_schema = DatasetParamSchema,
     func = cifar10_loader
 )
