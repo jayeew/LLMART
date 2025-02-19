0f53897fbf6830630767e3343326a96dbae4a779
jayeew <962326693@qq.com>
2025-02-19 09:40:01 +0800

    冲突测试

--- test.py
+++ test.py
@@ -29,4 +29,6 @@ if __name__=="__main__":
         myfunc_data = registry.get_data('cifar10_tool')
         data_loader = myfunc_data(**info['cifar10_tool'])
         print(type(data_loader))
+
+    print('本地修改')
     print('远端修改')
